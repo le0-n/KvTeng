9aeacf676477b70de81b4a4ab6df4641cfcf363c
le0-n <farrux.farxodov2000@gmail.com>
2023-02-15 22:21:36 +0500

    v1.1

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ let c = +prompt(`ax2+bx+c=0\n\nc = `)
 
 let D = b ** 2 - 4 * a * c
 
-if(D > 0 && a != 0) {
+if (D > 0 && a != 0) {
     let DxPlus = ((-b) + Math.sqrt(D)) / 2 * a 
     let DxMinus = ((-b) - Math.sqrt(D)) / 2 * a
     alert(`x1 = ${DxPlus}\tx2 = ${DxMinus}`)
-}else if(D == 0) {
+}else if (D == 0) {
     let x0 = (-b) / 2 * a
     alert(`x0 = ${x0}`)
+}else if (D < 0) {
+    alert(`D < 0, There aren't roots of this equation.`)
 }
